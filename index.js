d9af85a50e371a34188dabcdc5ed06797b12b7c2
cjengo033 <57130927+cjengo033@users.noreply.github.com>
2020-04-17 16:45:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var total = 4;
+var finalAverage = 0;
+var result = 0;
+
+function submitButton(){
+	var a = parseInt(document.getElementById('programmingSubject').value);
+	var b = parseInt(document.getElementById('mathSubject').value);
+	var c = parseInt(document.getElementById('programmingSubject').value);
+	var d = parseInt(document.getElementById('databaseSubject').value);
+	finalAverage =  a + b + c +d;
+	result = finalAverage / total;
+	if (result == 100)
+	{
+			return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is A+"; 
+	}
+	else if (result >= 90)
+	{
+			return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is A"; 
+	}
+	else if (result >= 80)
+	{	
+			return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is B"; 
+	}
+	else if (result >= 70)
+	{
+		return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is C";
+	}
+
+		else if (result >= 60)
+	{
+		return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is D";
+	}
+
+		else  (result < 59)
+	{
+		return document.getElementById('result').innerHTML = "Your final average is: " + result + " & And your letter grade is F";
+	}
+	
+}
